8950e1761fc8d802ba13e934f07b7601d1f8dc2c
Alejandro2097 <alejo12381@hotmail.es>
2021-09-21 18:15:45 -0500

    mongoose

--- models/server.js
+++ models/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 var cors = require('cors');
+const { dbConnection } = require('../database/config');
 const app = express()
 
 class Server{
@@ -7,12 +8,18 @@ class Server{
         this.app = express();
         this.port = process.env.PORT;
         this.usuariosPath = '/api/usuarios'
+
+        //Conectar a base de datos
+        this.conectarDB();
         //Midelwares
         //Funciones que siempre se ejecutan junto con el servidor.
        
         this.middlewares();
         //Rutas de mi aplicacion.
         this.routes();
+    }
+    async conectarDB(){
+        await dbConnection();
     }
     middlewares(){
          //CORS
