c9dc035fdbcfa90921f508fde30d07031c728382
Alejandro2097 <alejo12381@hotmail.es>
2021-09-21 21:02:40 -0500

    epam ahi voy

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,4 +1,5 @@
 const { response } = require('express');
+const Usuario = require('../models/usuario');
 
 const usuariosGet = (req = request, res = response) => {
 
@@ -22,11 +23,16 @@ const usuariosPut = (req, res) => {
         id
     });
 }
-const usuariosPost = (req, res) => {
-    const {nombre, edad} = req.body;
+const usuariosPost = async(req, res = response) => {
+    const body = req.body;
+
+    const usuario = new Usuario(body);
+
+    await usuario.save();
+
     res.json({
         'msg': 'post API - controlador',
-        nombre, edad
+        usuario
     });
 }
 const usuariosDelete = (req, res) => {
