d2f2c668ddc1c8b1bbd8ebacb07aea9146aa3fab
Alejandro2097 <alejo12381@hotmail.es>
2021-12-05 17:37:58 -0500

    control de archivos

--- models/server.js
+++ models/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const fileUpload = require('express-fileupload');
 
 const { dbConnection } = require('../database/config');
-const { auth } = require('google-auth-library');
 
 class Server {
 
@@ -16,6 +16,7 @@ class Server {
             categorias: '/api/categorias',
             productos:  '/api/productos',
             usuarios:   '/api/usuarios',
+            uploads:    '/api/uploads',
         }
         
         // Conectar a base de datos
@@ -35,6 +36,11 @@ class Server {
         this.app.use( express.json() );
         // Directorio Público
         this.app.use( express.static('public') );
+        //FileUpload - Carga de archivos
+        this.app.use(fileUpload({
+            useTempFiles : true,
+            tempFileDir : '/tmp/'
+        }));
     }
     routes() {
         this.app.use( this.paths.auth, require('../routes/auth'));
@@ -42,6 +48,7 @@ class Server {
         this.app.use( this.paths.categorias, require('../routes/categorias') );
         this.app.use( this.paths.productos, require('../routes/productos') );
         this.app.use( this.paths.usuarios, require('../routes/usuarios'));
+        this.app.use( this.paths.uploads, require('../routes/uploads'));
         
     }
     listen() {
